ac763dc311f0b9209427abcb7933c17fa05935d5
HonzaVrbensky <91247842+HonzaVrbensky@users.noreply.github.com>
2022-04-21 11:28:17 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+radio.setGroup(69)
+radio.setTransmitPower(7)
+radio.setTransmitSerialNumber(true)
+let my_serial = control.deviceSerialNumber()
+let name = "vote"
+let vote = 1
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    vote = (vote + 1) % 4
+    basic.showNumber(vote)
+})
+input.onButtonPressed(Button.B, function sending() {
+    radio.sendValue("vote", my_serial)
+})
