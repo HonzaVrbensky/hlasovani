3f118c15fb3ab9bf76ba97a0129737baf02b9a18
HonzaVrbensky <91247842+HonzaVrbensky@users.noreply.github.com>
2022-04-21 11:43:35 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def sending():
-    radio.send_value("vote", 1)
+    radio.send_value("vote", vote+1)
     music.play_tone(Note.C, music.beat(1000))
 input.on_button_pressed(Button.B, sending)
